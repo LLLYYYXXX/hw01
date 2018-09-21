60b293f16de84a1075939c96039bf4aee766aa16
LLLYYYXXX <1441569051@qq.com>
2018-09-21 23:27:23 +0800

    23

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ for i in range(1,10):
     for j in range(i, 10):
          
          formula = '{0:1}x{1:1}={2:<2}'.format(i, j, i*j)
-         print(formula, end=' ')
+         print (formula, end=' ')
     print()
